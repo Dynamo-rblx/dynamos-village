403780bde70be0aadaba3829e40238c54a639084
Dynamo <chickabamboombam@gmail.com>
2025-05-22 02:06:25 -0400

    Updated StarterGui/static/staticframe/static script.lua

---
+++ StarterGui/static/staticframe/static script.lua
@@ -0,0 +1,19 @@
+-- @ScriptType: Script
+while true do
+task.wait(0.065)
+script.Parent.Image = "http://www.roblox.com/asset/?id=4388380516"
+task.wait(0.065)
+script.Parent.Image = "http://www.roblox.com/asset/?id=4388381112"
+task.wait(0.065)
+script.Parent.Image = "http://www.roblox.com/asset/?id=4388381606"
+task.wait(0.065)
+script.Parent.Image = "http://www.roblox.com/asset/?id=4388382104"
+task.wait(0.065)
+script.Parent.Image = "http://www.roblox.com/asset/?id=4388382493"
+task.wait(0.065)
+script.Parent.Image = "http://www.roblox.com/asset/?id=4388382958"
+task.wait(0.065)
+script.Parent.Image = "http://www.roblox.com/asset/?id=4388383410"
+task.wait(0.065)
+script.Parent.Image = "http://www.roblox.com/asset/?id=4388383891"
+end
