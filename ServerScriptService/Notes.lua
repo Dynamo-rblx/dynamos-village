ec8ccda84c8aacf8a47a67d3f04e253aaf651ca3
Dynamo <chickabamboombam@gmail.com>
2025-05-22 02:06:10 -0400

    Updated ServerScriptService/Notes.lua

---
+++ ServerScriptService/Notes.lua
@@ -0,0 +1,26 @@
+-- @ScriptType: Script
+--[[ NOTES
+
+Easy:
+Add Horses
+Add Jeep-type thing
+Add weapons & tools
+
+Fix punching
+Make ghosts, zombies, and skeletons appear at night
+
+Make sense of inventory system
+
+Hard:
+Make terrain more diverse
+Add special buildings and landmarks
+Add generated trails and NPCs that walk along them
+Add treasure hunting
+Add wildlife
+Add fishing
+Add hunger
+Add achievements
+Make a story quest with a final battle
+Add crafting
+
+]]
